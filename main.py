68e013f62badc71ca2ca4d7fde785da31d724355
henriqueykajihara <henriqueykajihara@gmail.com>
2023-04-16 21:50:46 -0300

    Implementando separação dos tópicos e escrita em arquivo

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
 import read_pdf
 import os
+
 #********************************************************************************#
 def nivelValido(nivel):
     return ( nivel in ['0', '1', '2', '3'] )
 #********************************************************************************#
 def main_():
+    
     nivel = 99
     while nivel != '0':
+        read_pdf.cls()
 
         print('+--------------------------------------------------+')
         print('+--------- Processamento de artigos em PDF --------+')
@@ -19,32 +22,52 @@ def main_():
         print('+ 0 - Sair                                         +')
         print('+--------------------------------------------------+')
         nivel = input('Selecione o tema ou ZERO para sair: ')
-        print('+--------------------------------------------------+')
+        print('+--------------------------------------------------+\n')
         if nivelValido(nivel):
+            #diretorio_raiz_arquivos = "F:/9918_ia_trabalho2/arquivos/"
+            diretorio_raiz_arquivos = os.getcwd()+"/arquivos/"
+            diretorio_txt = "F:/9918_ia_trabalho2/txt/"
             if nivel == '1':
-                opcao = "F:/9918_ia_trabalho2/arquivos/image/"
-                destino_txt = "F:/9918_ia_trabalho2/txt/image/"
-                print('Nivel 1111111111')
+                opcao_selecionada_string = '1 - Processamento de imagens'
+                opcao = diretorio_raiz_arquivos+"image/"
+                destino_txt = diretorio_txt + "image/"
             if nivel == '2':
-                opcao = "F:/9918_ia_trabalho2/arquivos/ia/"
-                destino_txt = "F:/9918_ia_trabalho2/txt/ia/"
+                opcao_selecionada_string = '2 - Inteligência artificial'
+                opcao = diretorio_raiz_arquivos+"ia/"
+                destino_txt = diretorio_txt+"ia/"
             if nivel == '3':
-                opcao = "F:/9918_ia_trabalho2/arquivos/machine_learning/"
-                destino_txt = "F:/9918_ia_trabalho2/txt/machine_learning/"
+                opcao_selecionada_string = '3 - Machine Learning'
+                opcao = diretorio_raiz_arquivos+"machine_learning/"
+                destino_txt = diretorio_txt+"machine_learning/"
+
             if nivel == '0':
                 opcao = '0'
 
             if os.path.exists(opcao):
-                os.system('cls')
-                read_pdf.processa_retorna_array_arquivos( opcao, destino_txt )
+                read_pdf.cls()
+                print('+--------------------------------------------------+')
+                print('Você selecionou a opção: ' + opcao_selecionada_string )
+                print('+--------------------------------------------------+')
+                print('+ 1 - Processar artigos pdf                        +')
+                print('+ 2 - Page Rank                                    +')
+                print('+ 0 - Voltar                                       +')
+                print('+--------------------------------------------------+')
+                opcao_processamento = input('Selecione a opção ou ZERO para sair: ')
+                if opcao_processamento == '1':
+                    read_pdf.processa_transforma_arquivos_pdf( opcao, destino_txt )
+                elif opcao_processamento == '2':
+                    read_pdf.page_rank(destino_txt)
+                elif opcao_processamento == '0':
+                    nivel = 99
+                    continue
             else:
                 if opcao == '0':
-                    print(" --- Fim do programa :) ---")
+                    print(" --- Fim do programa ---")
                     return
                 else:
                     print("Arquivo " + opcao +" nao encontrado!")                   
         else:
-            os.system('cls')
+            read_pdf.cls()
             print('Opção ',nivel,' invalida!')
         #os.system('cls')
 
@@ -54,4 +77,4 @@ def main():
 #********************************************************************************#
 if __name__ == "__main__":
     
-    main()
+    main_()
--- read_pdf.py
+++ read_pdf.py
@@ -1,11 +1,11 @@
-import PyPDF2 as pypdf #pip install PyPDF2
+import PyPDF2 as pypdf
 import os
+import spacy
+from spacy.lang.en import English
+from spacy.matcher import PhraseMatcher
 
 POSICAO_PALAVRA  = 0
 POSICAO_CONTADOR = 1
-#********************************************************************************#
-def processa_retorna_array_arquivos(caminho, destino_txt):
-    processa_transforma_arquivos_pdf(caminho, destino_txt)
 
 #********************************************************************************#
 def processa_transforma_arquivos_pdf(caminho, destino_txt):
@@ -14,70 +14,151 @@ def processa_transforma_arquivos_pdf(caminho, destino_txt):
     arquivos_pdf = pega_arquivos_pdf(caminho) 
     if not cria_diretorio(destino_txt):
         return
-    
+    arquivos_sucesso = []
+    #pagina_referencias = -1
     palavras_recorrentes = []
-    top10_palavras_recorrentes = []    
     for arquivo_pdf in arquivos_pdf:
+        palavras_recorrentes_arquivo = []
         print("##############################################################")
         with open(caminho+arquivo_pdf, 'rb') as pdf_aberto:
-            print("Lendo arquivo: "+ arquivo_pdf)
-            pagina_referencias = -1
-            inicio_pagina_objective = -1
-            inicio_pagina_problem = -1
-            inicio_pagina_method = -1
-            inicio_pagina_contributes = -1
-
+            print("Lendo arquivo: "+ arquivo_pdf +"\n")
+            
             arquivo_lido = pypdf.PdfReader(pdf_aberto)
             total_paginas = len(arquivo_lido.pages)
             lista_texto_arquivo = []
             
+            texto_inteiro = ''            
             for numero_pagina in range(total_paginas):
             
                 texto = arquivo_lido.pages[numero_pagina].extract_text()
                 lista_texto_arquivo.append(texto.splitlines())
-                #for pagina in lista_texto_arquivo:
-                #    for linha in pagina:
-                #        adiciona_lista_palavras(palavras_recorrentes, linha)
-                #if is_possivel_referencia(texto):
-                #    pagina_referencias = numero_pagina
-                #    print("Referencia "+arquivo_pdf+" começa na pagina: "+ str(pagina_referencias+1))
-                #    adiciona_lista_palavras = False
-                #else:
-                #    for pagina in lista_texto_arquivo:
-                #        for linha in pagina:
-                #            adiciona_lista_palavras(palavras_recorrentes, linha)
-                if is_possivel_referencia(texto):
-                    pagina_referencias = numero_pagina
-                    print("Referencia "+arquivo_pdf+" começa na pagina: "+ str(pagina_referencias+1))
-                else:
-                    if pagina_referencias > 0:
-                        for pagina in lista_texto_arquivo:
-                            for linha in pagina:
-                                adiciona_lista_palavras(palavras_recorrentes, linha)
+                texto_inteiro += texto
 
             texto_referencias = ''    
-            if pagina_referencias != -1:
-                for numero_pagina in range(pagina_referencias, total_paginas):
-                    texto_referencias += arquivo_lido.pages[numero_pagina].extract_text()
+            texto_introducao = ''              
+            is_referencia = False 
+            is_introducao = False   
+            for pagina in lista_texto_arquivo:
+                for linha in pagina:
 
-            nome_arquivo = pega_nome_arquivo(arquivo_lido, destino_txt)
-            
-            print(nome_arquivo)
-            with open(nome_arquivo, 'w', encoding='utf-8') as novo_arquivo:
-                for linha_split in lista_texto_arquivo:
-                    for linha in linha_split:
-                        adiciona_lista_palavras(top10_palavras_recorrentes, linha)
-#
-                        novo_arquivo.write(linha + ";;")
-                        novo_arquivo.write("\n")
-        print("\n")
-                
-    top10_palavras_recorrentes = rank_top10(palavras_recorrentes)
-    for palavra in top10_palavras_recorrentes:
-        print(palavra)                
-        
+                    if  is_possivel_referencia(linha):
+                        is_referencia = True
+
+                    if not is_referencia:
                         
+                        #RETIRANDO A INTRODUCAO DO TEXTO
+                        if is_possivel_introducao(linha):
+                            #print("INICIO INTRO: " + linha)
+                            is_introducao = True
+
+                        if is_introducao and is_possivel_final_introducao(linha):
+                            #print("FINAL INTRO: " + linha)
+                            is_introducao = False
+                        if is_introducao:
+                            texto_introducao += linha + '\n'
+#ESCREVE TODO O TEXTO NESTE PONTO
+                        texto_inteiro += linha+ '\n'
+                        adiciona_lista_palavras(palavras_recorrentes, linha)
+                        adiciona_lista_palavras(palavras_recorrentes_arquivo, linha)
+                    else:
+#ADICIONA O TEXTO DE REFERÊNCIAS SÓ PARA TESTAR DEPOIS
+                        texto_referencias += linha + '\n'
+
+            encontrou_tudo = True
+            objetivo = retorna_informacoes_objetivo(texto_introducao, texto_inteiro)
+            if lista_vazia(objetivo):
+                objetivo.append("OBJECTIVE - INFORMATION NOT FOUND")
+                print("OBJECTIVE - INFORMATION NOT FOUND")
+                encontrou_tudo = False
+
+            problema = retorna_informacoes_problemas(texto_introducao, texto_inteiro)
+            if lista_vazia(problema):
+                problema.append("PROBLEM - INFORMATION NOT FOUND")
+                print("PROBLEM - INFORMATION NOT FOUND")
+                encontrou_tudo = False
+
+            metodologia = retorna_informacoes(texto_inteiro, ["method", "interviews, survey, content, analysis"])
+            if lista_vazia(metodologia):
+                metodologia.append("METHOD - INFORMATION NOT FOUND")
+                print("METHOD - INFORMATION NOT FOUND")
+                encontrou_tudo = False
+
+            contribuicao = retorna_informacoes(texto_inteiro, ["Contributions", "contributes to", "contribuitions"])
+            if lista_vazia(contribuicao):
+                contribuicao.append("CONTRIBUTES TO - INFORMATION NOT FOUND")
+                print("CONTRIBUTES TO - INFORMATION NOT FOUND")
+                encontrou_tudo = False
+
+            texto_formatado = formata_informacoes_para_escrever(objetivo, problema, metodologia, contribuicao)
+            nome_arquivo = pega_nome_arquivo(arquivo_lido, destino_txt)
+            escreve_conteudo_em_arquivo_txt(nome_arquivo, texto_formatado)
+            if encontrou_tudo:
+                arquivos_sucesso.append(nome_arquivo)
+            
+    mostra_top_10(palavras_recorrentes)
+                  
+    print( str(len(arquivos_sucesso))+' arquivos lidos com sucesso')
+    for arquivo in arquivos_sucesso:
+        print(arquivo)
+
     return arquivos_processados
+
+#********************************************************************************#
+def formata_informacoes_para_escrever(objetivo, problema, metodologia, contribuicao):
+    texto_formatado = 'Objetivo: '+ objetivo[0] + ';;\n'
+    texto_formatado += 'Problema: '+ problema[0] + ';;\n'
+    texto_formatado += 'Metodologia: '+ metodologia[0] + ';;\n'
+    texto_formatado += 'Contribuicao: '+ contribuicao[0] + ';;\n'
+
+    return texto_formatado
+#********************************************************************************#
+def retorna_informacoes_problemas(texto_introducao, texto_inteiro):
+
+    lista_problemas = ["limitation", "issue", "problem", "challenge", "dilemma", "obstacle", "difficulty"]
+    problema_sentencas = retorna_informacoes(texto_introducao, lista_problemas)
+    if lista_vazia(problema_sentencas):
+        problema_sentencas = retorna_informacoes(texto_inteiro ,lista_problemas)
+    return problema_sentencas
+
+#********************************************************************************#
+def retorna_informacoes_objetivo(texto_introducao, texto_inteiro):
+    
+    lista_objetivos = [ "objective", "purposed", "purpose", "intention" ]
+    objetivos_sentencas = retorna_informacoes(texto_introducao, lista_objetivos)
+    if lista_vazia(objetivos_sentencas):
+        objetivos_sentencas = retorna_informacoes(texto_inteiro ,lista_objetivos)
+    return objetivos_sentencas
+    
+#********************************************************************************#
+def retorna_informacoes(texto_introducao, lista_palavras_referencia):
+
+    nlp = spacy.load("en_core_web_sm")
+    documento = nlp(texto_introducao)
+    matcher = PhraseMatcher(nlp.vocab, attr="LOWER")
+    
+    lista_patterns = []
+    for palavra_referencia in lista_palavras_referencia:
+        lista_patterns.append(nlp(palavra_referencia))
+              
+    for palavra_referencia in lista_palavras_referencia:
+        matcher.add(palavra_referencia, None, *lista_patterns)
+    
+    sentencas = []
+    for sentenca in documento.sents:
+        if matcher(nlp(sentenca.text)):
+            sentencas.append(sentenca.text)
+            if len(sentencas) > 0:
+                break
+
+    return sentencas
+
+#********************************************************************************#
+def escreve_conteudo_em_arquivo_txt(nome_arquivo, texto_inteiro):
+
+    with open(nome_arquivo, 'w', encoding='utf-8') as novo_arquivo:
+        for linha in texto_inteiro:
+            novo_arquivo.write(linha)
+        #novo_arquivo.write(";;")
 
 #********************************************************************************#
 def rank_top10(lista_palavras):
@@ -93,12 +174,20 @@ def rank_top10(lista_palavras):
             return top_10
 
 #********************************************************************************#
+def mostra_top_10(palavras_recorrentes):
+
+    top10_palavras_recorrentes = rank_top10(palavras_recorrentes)
+    print('Rank de palavras mais encontradas:')
+    for palavra in top10_palavras_recorrentes:
+        print(palavra)               
+
+#********************************************************************************#
 def adiciona_lista_palavras(lista_palavras, linha):
     
     lista_palavras_linha = linha.split()
     for palavra in lista_palavras_linha:
 
-        palavra_verificar = replace_caracteres_invalido(palavra, "")
+        palavra_verificar = replace_caracteres_invalido(palavra.lower(), "")
         if len(palavra_verificar) > 3 and not is_palavra_irrelevante(palavra_verificar):
             if not palavra_adicionada(lista_palavras, palavra_verificar):
                 lista_palavras.append([ palavra_verificar, 1])
@@ -155,7 +244,10 @@ def is_palavra_irrelevante(palavra):
     for preposicao in lista_preposicoes:
         if palavra.upper() == preposicao.upper():
             return True
-
+        
+    if palavra.upper() == 'IEEE':
+        return True
+    
     return False
 
 #********************************************************************************#
@@ -173,20 +265,20 @@ def palavra_adicionada(lista_palavras, palavra):
     return False
 
 #********************************************************************************#
-def is_possivel_abstract(texto):
-    return ('Abstract' in texto )
-
-#********************************************************************************#
-def is_possivel_index_Terms(texto):
-    return ('Index Terms' in texto )
-
-#********************************************************************************#
-def is_possivel_objective(texto):
-    return ('Objective' in texto )
-
-#********************************************************************************#
 def is_possivel_referencia(texto):
-    return ('References' in texto ) or ('REFERENCES' in texto) or ("\nR\nEFERENCES" in texto)
+    return ('References' in texto ) or ('REFERENCES' in texto) or ("\nR\nEFERENCES" in texto) or ("EFERENCES" in texto)
+
+#********************************************************************************#
+def is_possivel_introducao(texto):
+    return ('INTRODUCTION' in texto)
+
+#********************************************************************************#
+def is_possivel_final_introducao(texto):
+    return ('A.' in texto) or ('II.' in texto)
+
+#********************************************************************************#
+def empty_text(text):
+    return text == ''
 
 #********************************************************************************#
 def cria_diretorio(diretorio_destino):
@@ -210,7 +302,7 @@ def pega_nome_arquivo(arquivo_lido, destino_txt):
     if not os.path.exists(destino_txt):
         os.makedirs(destino_txt)
     texto_primeira_pagina = arquivo_lido.pages[0].extract_text()
-    texto_primeira_linha = texto_primeira_pagina.split('\n')[0]
+    texto_primeira_linha = texto_primeira_pagina.split('\n')[1]
     return destino_txt + replace_caracteres_invalido(texto_primeira_linha, "_") +'.txt'
 
 #********************************************************************************#
@@ -223,15 +315,14 @@ def replace_caracteres_invalido(texto_arquivo, string_replace):
     novo_texto = novo_texto.replace( "]", string_replace )
     novo_texto = novo_texto.replace( "(", string_replace )
     novo_texto = novo_texto.replace( ")", string_replace )
+    novo_texto = novo_texto.replace( "/", string_replace )
+    
     return novo_texto
 
 #********************************************************************************#
 def processa_paginas_pdf(arquivo_lido):
     sessoes = arquivo_lido.outline()
     print(sessoes)
-    #for sessao in sessoes:
-    #    titulo = sessao.title 
-    #    print(f"Teste:"'{titulo}')
 
 #********************************************************************************#
 def pega_arquivos_pdf(caminho):
@@ -248,3 +339,15 @@ def pega_arquivos_pdf(caminho):
 #********************************************************************************#
 def is_pdf(arquivo):
     return '.pdf' in arquivo
+
+#********************************************************************************#
+def lista_vazia(lista):
+    return len(lista) == 0
+
+#********************************************************************************#
+def page_rank():
+    return print("ta pra fazer")
+
+#********************************************************************************#
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
